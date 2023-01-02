93a0704869d850e4aab60a0f680d2a14db2b88d0
hp <purvravani06@gmail.com>
2023-01-02 15:08:04 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(home: ,));
+  runApp(MaterialApp(home:admob() ,));
+}
+
+class admob extends StatefulWidget {
+  const admob({Key? key}) : super(key: key);
+
+  @override
+  State<admob> createState() => _admobState();
+}
+
+class _admobState extends State<admob> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(appBar: AppBar(centerTitle: true,title: Text("ADMOB"),),));
+  }
 }
